abcdec7b06b3299f00b142d21c1ef74913ca1366
Andy Bett <andybett99@gmail.com>
2025-04-04 23:54:47 +0300

    page view builder added

--- lib/Screens/mainScreen.dart
+++ lib/Screens/mainScreen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:funfacts/Screens/settings_screen.dart';
+import 'package:dio/dio.dart';
 
 class Mainscreen extends StatefulWidget {
   const Mainscreen({super.key});
@@ -9,34 +12,35 @@ class Mainscreen extends StatefulWidget {
 }
 
 class _MainscreenState extends State<Mainscreen> {
-  List<String> facts = [
-    "Cows have best friends and can become stressed when they are separated.",
-    "Honey never spoils. It can last for centuries without going bad.",
-    "A group of owls is called a parliament.",
-    "Bananas are berries, but strawberries aren't.",
-    "The Eiffel Tower can be 15 cm taller during the summer due to thermal expansion.",
-    "The average person laughs about 15 times a day.",
-    "A single cloud can weigh as much as 1 million pounds.",
-    "Your taste buds have a lifespan of about 10 to 14 days.",
-    "The longest recorded hiccuping lasted for 68 years!",
-    "Bees can recognize human faces.",
-    "The world's largest desert is Antarctica.",
-    "A day on Venus is longer than a year on Venus.",
-    "Butterflies taste with their feet.",
-    "The dot over the letter 'i' is called a tittle.",
-    "A group of flamingos is called a 'flamboyance.'",
-    "The shortest war in history was between Britain and Zanzibar, lasting only 38 minutes.",
-    "The smell of freshly-cut grass is actually a plant distress call.",
-    "Elephants are the only animals that can't jump.",
-    "There are more possible iterations of a game of chess than there are atoms in the observable universe.",
-    "The world's oldest toy is a stick, according to archaeological finds."
-  ];
+  List<dynamic> facts = [];
+  bool isLoading = true;
+
+  void getData() async {
+    try {
+      Response response = await Dio().get(
+          "https://raw.githubusercontent.com/AndyBett/flutter_dummy_api/refs/heads/master/facts.json");
+
+      facts = jsonDecode(response.data);
+      isLoading = false;
+
+      setState(() {});
+    } catch (e) {
+      isLoading = false;
+    }
+  }
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+  // api url https://raw.githubusercontent.com/AndyBett/flutter_dummy_api/refs/heads/master/facts.json
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Fun Facts"),
+        title: const Text("Fun Facts"),
         centerTitle: true,
         actions: [
           Padding(
@@ -44,29 +48,31 @@ class _MainscreenState extends State<Mainscreen> {
             child: InkWell(
                 onTap: () {
                   Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return SettingsScreen();
+                    return const SettingsScreen();
                   }));
                 },
-                child: Icon(Icons.settings)),
+                child: const Icon(Icons.settings)),
           )
         ],
       ),
       body: Column(children: [
         Expanded(
-          child: PageView.builder(
-              itemCount: facts.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Center(
-                    child: Text(
-                  facts[index],
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 35),
-                ));
-              }),
+          child: isLoading
+              ? Center(child: const CircularProgressIndicator())
+              : PageView.builder(
+                  itemCount: facts.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Center(
+                        child: Text(
+                      facts[index],
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 35),
+                    ));
+                  }),
         ),
         Container(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
             child: Text("Swipe left for more"),
           ),
         )
