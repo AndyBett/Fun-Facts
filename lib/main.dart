6f625b4caef0c9a622f1bb8a53446243771e6217
Andy Bett <andybett99@gmail.com>
2025-04-06 22:25:31 +0300

    Using Provider for Global State Management

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:funfacts/Providers/themeProvider.dart';
 import 'package:funfacts/Screens/mainScreen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ChangeNotifierProvider(
+      create: (context) {
+        return ThemeProvider();
+      },
+      child: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -15,9 +21,12 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    var themeProvider = Provider.of<ThemeProvider>(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(useMaterial3: true),
+      theme: themeProvider.isDarkModeChecked
+          ? ThemeData.dark(useMaterial3: true)
+          : ThemeData.light(useMaterial3: true),
       home: Mainscreen(),
     );
   }
